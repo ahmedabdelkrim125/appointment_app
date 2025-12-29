df02380fbf921655e9150dd0a0818c658027b5cb
ahmedabdelkrim125 <ahmed125abdelkrim@gmail.com>
2025-12-29 14:29:15 +0200

    Trigger Flovers Prodaction and Development , Add Fastlane to Android and connected Firbase Distribution Bumped version 1.0.1 ✅

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application")
+    id("com.google.gms.google-services")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
@@ -25,6 +25,22 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+    }
+
+    flavorDimensions += "environment"
+
+    productFlavors {
+        create("development") {
+            dimension = "environment"
+            applicationIdSuffix = ".dev"
+            versionNameSuffix = "-dev"
+            resValue("string", "app_name", "Appointment Dev")
+        }
+
+        create("production") {
+            dimension = "environment"
+            resValue("string", "app_name", "Appointment App")
+        }
     }
 
     buildTypes {
